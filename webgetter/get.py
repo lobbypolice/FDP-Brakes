43dfa59fa93a83f1da8ebf061ef9244dd966787f
kshammer <hammerschmiks@vcu.edu>
2017-10-26 21:56:23 -0400

    fixed get

--- webgetter/get.py
+++ webgetter/get.py
@@ -1,5 +1,5 @@
 import requests
 
-r = requests.get('http://www.autozone.com/')
+r = requests.get('https://shop.advanceautoparts.com/web/PartSearchCmd?storeId=10151&catalogId=10051&langId=-1&pageId=partTypeList&actionSrc=Form&searchTerm=gnad999')
 print(r.status_code)
 print(r.text)
