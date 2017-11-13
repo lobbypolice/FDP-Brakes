70755930b6b3f44ffd33b81d13e75bbb7b2277f9
Kyle Hammerschmidt <Kyle Hammerschmidt>
2017-11-13 14:12:46 -0500

    removed non working files

--- test/search.py
+++
@@ -1,6 +0,0 @@
-import requests
-from lxml import html
-session_requests = requests.session()
-url = "https://shop.advanceautoparts.com/"
-result = session_requests.get(url)
-tree = html.fromstring(result.text)
--- webgetter/test.py
+++
@@ -1,5 +0,0 @@
-import requests
-#http://www.autozone.com/searchresult?searchText=MKD1204&vehicleSetFromSearch=false&keywords=MKD1204
-url = "http://www.autozone.com/searchresult?searchText=MKD1204&vehicleSetFromSearch=false&keywords=MKD1204"
-r = requests.get("http://www.autozone.com/searchresult?searchText=MKD1204&vehicleSetFromSearch=false&keywords=MKD1204", allow_redirects=False)
-print(r.headers)
