82548c5a436d77645acf32e1c32cc73f49332848
kshammer <hammerschmiks@vcu.edu>
2017-11-14 18:07:50 -0500

    fixed advancedauto url generator and added autozone spider

--- FDPBrakes/FDPBrakes/spiders/advancedAuto.py
+++ FDPBrakes/FDPBrakes/spiders/advancedAuto.py
@@ -2,7 +2,7 @@ import scrapy
 
 class advancedAuto(scrapy.Spider):
     name = "advancedAuto"
-    start_urls = ["https://shop.advanceautoparts.com/web/PartSearchCmd?storeId=10151&catalogId=10051&pageId=partTypeList&suggestion=&actionSrc=Form&langId=-1&categoryId=15694&beginIndex=0&sortBy=5&_r=0.8193827588178413#",]
+    start_urls = []
     
 
     def parse(self, response):
@@ -10,5 +10,8 @@ class advancedAuto(scrapy.Spider):
             yield{
                 'modelNumber': model.css('span.aap-pl-item__prno::text').extract_first(),
                 'name': model.css('h3.aap-pl-item__pname::text').extract_first(),
-                'price': model.css('span.aap-pl-item__price::text').extract_first(), 
+                'price': model.css('span.aap-pl-item__price::text').extract_first()[2:-2], 
             }
+    def __init__(self):
+        for i in range(0, 11840, 10):
+            self.start_urls.append("https://shop.advanceautoparts.com/web/PartSearchCmd?storeId=10151&catalogId=10051&pageId=partTypeList&suggestion=&actionSrc=Form&langId=-1&categoryId=15694&beginIndex=" + str(i) + "&sortBy=4&_r=0.03323842671958288#")
