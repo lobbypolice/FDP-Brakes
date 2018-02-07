7525981c5d17d744edc96ef4b51221ad2b5bc61d
kshammer <hammerschmiks@vcu.edu>
2018-02-07 13:40:55 -0500

    fixed casper

--- PhantomTest/casperTest.js
+++ PhantomTest/casperTest.js
@@ -2,7 +2,7 @@ var casper = require('casper').create();
 var links;
 
 function getLinks() {
-    // Scrape the links from top-right nav of the website
+// Scrape the links from top-right nav of the website
     var links = document.querySelectorAll('ul.navigation li a');
     return Array.prototype.map.call(links, function (e) {
         return e.getAttribute('href')
@@ -16,9 +16,10 @@ casper.then(function () {
     links = this.evaluate(getLinks);
 });
 
-casper.run(function () {
-    for (var i in links) {
+casper.then(function () {
+    for(var i in links) {
         console.log(links[i]);
     }
-    casper.done();
 });
+
+casper.run();
