01f6ba9298465641dc3a0962776c35859ac39c52
kshammer <hammerschmiks@vcu.edu>
2018-04-26 00:11:09 -0400

    got autozone wokring

--- FDPBrakes/FDPBrakes/spiders/autozone.py
+++ FDPBrakes/FDPBrakes/spiders/autozone.py
@@ -1,21 +1,23 @@
 import scrapy
 
+
 class advancedAuto(scrapy.Spider):
     name = "Autozone"
     start_urls = []
-    
 
     def parse(self, response):
         for model in response.css('div.shelfItemDetails'):
             yield{
-                'modelNumber': model.css('span').extract(),
-                'type': model.css('li.prodNotes').extract(),
-                'price': model.css('div.price').extract(), 
+                'modelNumber': model.xpath('//*[contains(@property,\'mpn\')]/text()').extract_first(),
+                'type': model.xpath('//*[contains(@class,\'prodName\')]/text()').extract_first(),
+                'price': model.xpath('//*[contains(@class,\'price\')]//strong').extract_first(),
             }
-            #aap-pl-js-itemlist > div:nth-child(1)
+
     def __init__(self):
         for i in range(1, 445):
-            self.start_urls.append("https://www.autozone.com/brakes-and-traction-control/brake-pads?pageNumber=" + str(i))
-#//*[@id="item_details_298604_0"]/ul/li[2]/em/span
-#//*[@id="item_details_202573_1"]/ul/li[2]/em/span
-#<span property="mpn">27111</span>
+            self.start_urls.append(
+                "https://www.autozone.com/brakes-and-traction-control/brake-pads?pageNumber=" + str(i))
+# //*[@id="item_details_298604_0"]/ul/li[2]/em/span
+# //*[contains@class,'prodImg')]//a/@href
+
+#
