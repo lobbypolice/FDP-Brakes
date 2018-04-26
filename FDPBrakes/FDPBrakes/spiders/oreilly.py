9d9271b7e2c9eb9ac8e3f1b8149a582d2791ae36
kshammer <hammerschmiks@vcu.edu>
2018-04-26 02:32:13 -0400

    some of it works

--- FDPBrakes/FDPBrakes/spiders/oreilly.py
+++ FDPBrakes/FDPBrakes/spiders/oreilly.py
@@ -16,10 +16,10 @@ class napa(scrapy.Spider):
             x = x[2:-2]
             self.start_urls.append("https://www.oreillyauto.com/search?q=" + x)
     def parse(self, response):
-        for model in response.css('div.product-container'):
+        for model in response.css('div.product_block2'):
             yield{
-                'modelNumber' : model.css( 'dd.item-number:text').extract_first(),
-                'name' : model.css('a.js-product-link::text').extract_first(),
+                'modelNumber' : model.xpath('//*[contains(@class,\'item-number\')]/text()').extract_first(),
+                'name' : model.xpath('//*[contains(@class,\'product_title\')]').extract_first(),
                 'material' : model.xpath('//*[contains(@class,\'attribute_wrap\')]/span/strong/text()').extract_first(),
             }
             #productListItemADOAD7822_203472555 > div.listing-content > div.listing-detail > div.listing-detail-item.listing-detail-item-part > div.listing-detail-text.listing-detail-text-par
